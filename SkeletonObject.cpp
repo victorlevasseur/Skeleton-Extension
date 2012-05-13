6e764befcf126548f8c408aabd58ebd78790ab6f
Victor Levasseur <victorlevasseur01@orange.fr>
2012-05-13 15:59:40 +0200

    Added licence to all source files

--- SkeletonObject.cpp
+++ SkeletonObject.cpp
@@ -1,3 +1,23 @@
+/*
+Copyright (C) 2012 Victor Levasseur
+
+  This software is provided 'as-is', without any express or implied
+  warranty.  In no event will the authors be held liable for any damages
+  arising from the use of this software.
+
+  Permission is granted to anyone to use this software for any purpose,
+  including commercial applications, and to alter it and redistribute it
+  freely, subject to the following restrictions:
+
+  1. The origin of this software must not be misrepresented; you must not
+     claim that you wrote the original software. If you use this software
+     in a product, an acknowledgment in the product documentation would be
+     appreciated but is not required.
+  2. Altered source versions must be plainly marked as such, and must not be
+     misrepresented as being the original software.
+  3. This notice may not be removed or altered from any source distribution.
+  */
+
 #include <SFML/Graphics.hpp>
 #include "GDL/Object.h"
 
@@ -18,26 +38,6 @@
 SkeletonObject::SkeletonObject(std::string name_) :
 Object(name_), skeleton()
 {
-
-
-    /*{
-        Bone *newBone = new Bone("first", &bone);
-        newBone->SetSize(100);
-        newBone->SetRotation(20);
-    }
-    {
-        Bone *newBone = new Bone("second", &bone);
-        newBone->SetSize(150);
-        newBone->SetRotation(45);
-
-        {
-            Bone *newBone2 = new Bone("subsecond", newBone);
-            newBone2->SetSize(75);
-            newBone2->SetRotation(10);
-        }
-    }*/
-
-
 
 }
 
