5b9c90418149427e7504428c3769c27ff47c4877
victorlevasseur <victorlevasseur01@orange.fr>
2012-08-24 13:20:09 +0200

    Rename source code files (4)

---
+++ ImageManager.h
@@ -0,0 +1,49 @@
+#ifndef IMAGEMANAGER_H
+#define IMAGEMANAGER_H
+
+#include <map>
+#include <queue>
+#include <string>
+
+#include <boost/shared_ptr.hpp>
+#include <SFML/Graphics/Texture.hpp>
+
+class RuntimeScene;
+class ImageManager;
+class SFMLTextureWrapper;
+
+namespace gd {class ArbitraryResourceWorker;}
+
+namespace Sk
+{
+namespace Res
+{
+
+class SkImageManager
+{
+    public:
+        SkImageManager();
+        ~SkImageManager();
+
+        void UnloadAllImages();
+        void ExposeImage(const std::string &picture);
+
+        void LoadResources(const RuntimeScene & scene, const ImageManager & imageMgr);
+
+        boost::shared_ptr<SFMLTextureWrapper> GetImage(const std::string &name);
+
+        #ifdef GD_IDE_ONLY
+        void ShowResourcesToGD(gd::ArbitraryResourceWorker & worker);
+        #endif
+
+    protected:
+    private:
+        std::queue<std::string> m_needToBeLoaded;
+        std::map<std::string, boost::shared_ptr<SFMLTextureWrapper> > m_images;
+};
+
+}
+
+}
+
+#endif // IMAGEMANAGER_H
